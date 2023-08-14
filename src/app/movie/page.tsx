60d30880806d19c8072aa6c3aa1e08de6f55a16b
Artsiom Voitas <artsiom.voitas@gmail.com>
2023-08-14 18:11:04 +0300

    fix: changed condition of rendering

--- src/app/movie/page.tsx
+++ src/app/movie/page.tsx
@@ -28,5 +28,5 @@ export default function Page() {
     }
   }, [dispatch, imdbID, movie.Title])
 
-  return <>{!isLoading ? <Loader /> : <MovieCard movie={movie} />}</>
+  return <>{isLoading ? <Loader /> : <MovieCard movie={movie} />}</>
 }
